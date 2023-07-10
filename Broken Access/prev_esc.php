24868784db30942ed76a1af06810dbd9d44806b6
BARBAROSSA <124465708+omarelqady@users.noreply.github.com>
2023-07-10 05:09:49 +0300

    Add files via upload

---
+++ Broken Access/prev_esc.php
@@ -0,0 +1,27 @@
+<?php
+
+$role = 'user';
+
+// Set the role value in a cookie
+setcookie('role', $role, time() + (86400 * 30), '/'); // Cookie expires in 30 days
+
+// Retrieve the role from the cookie (if set)
+if (isset($_COOKIE['role'])) {
+    $role = $_COOKIE['role'];
+}
+if($role==='admin')
+{
+    echo "Admin and role is: " . $role;
+    //or Admin content
+}
+else if($role==='user')
+{
+    echo "User and role is: " . $role;
+    //or user content
+}
+else
+{
+    echo "NO SUCH ROLE";
+}
+
+?>
